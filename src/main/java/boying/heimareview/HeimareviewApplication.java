042136ba38360c4d2dc87ca833d2f32be721c899
Boying <1090452605@qq.com>
2023-12-07 02:53:03 +0000

    init

---
+++ src/main/java/boying/heimareview/HeimareviewApplication.java
@@ -0,0 +1,13 @@
+package boying.heimareview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeimareviewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HeimareviewApplication.class, args);
+    }
+
+}
---
+++ src/test/java/boying/heimareview/HeimareviewApplicationTests.java
@@ -0,0 +1,13 @@
+package boying.heimareview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeimareviewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
